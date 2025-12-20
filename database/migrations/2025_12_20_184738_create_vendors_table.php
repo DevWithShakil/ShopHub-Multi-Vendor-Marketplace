668f73e876df6d4559bb5be1077fbd0cbe7cc8ed
DevWithShakil <m.shakil720@gmail.com>
2025-12-21 00:52:31 +0600

    Vendors and categories migration done

---
+++ database/migrations/2025_12_20_184738_create_vendors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('shop_name');
+            $table->string('slug')->unique();
+            $table->string('logo')->nullable();
+            $table->string('banner')->nullable();
+            $table->text('description')->nullable();
+            $table->string('address')->nullable();
+            $table->decimal('commission_rate', 5, 2)->default(10.00);
+            $table->boolean('is_active')->default(true);
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
